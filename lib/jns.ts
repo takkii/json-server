d30f222aa09d0cf2203ded25c9c028bc52320ec7
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-05-22 13:00:09 +0900

    Create.

---
+++ lib/jns.ts
@@ -0,0 +1,15 @@
+import { execSync } from 'node:child_process';
+
+class Env {
+    async run() {
+        try {
+            const stdout = execSync('ts-node server.ts 1337 cdae648a-7d2f-40c8-bcc8-297335e27f67')
+            console.log(`stdout: ${stdout.toString()}`)
+        } catch (error) {
+            console.error('Error: ', error);
+        }
+    }
+}
+
+const environ = new Env();
+environ.run().then();
